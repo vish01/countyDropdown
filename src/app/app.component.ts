f238f3c05e62c6700343a483694f8c742a813e54
vish01 <vishallimbachiya379@gmail.com>
2018-12-16 14:58:25 -0500

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'county-dropdown';
+  title = 'JSON to Table Example';
+  constructor (private httpService: HttpClient) { }
+  arrBirds: string [];
+  arrBirdsRegion: string [];
+arrSort: string [];
+  ngOnInit () {
+    this.httpService.get('https://api.myjson.com/bins/dbg52').subscribe(
+      data => {
+        // console.log(data);
+        this.arrBirds = data as string [];	 // FILL THE ARRAY WITH DATA.
+    //     for(let i = 0; i< this.arrBirds.length; i++) {
+    //       //the property after data[i]. needs to match the exact name that is on your JSON file... So, name is a different property than Name
+    //       this.arrBirds.push({label: data[i].name, value: data[i].level});
+    //  }
+       for(let i=0; i<= this.arrBirds.length; i++)
+       {
+         if(this.arrBirds[i] =="New York")
+         {
+           console.log("FOUND");
+         }
+         else{
+           console.log("Not Found");
+         }
+       }
+
+        this.arrBirdsRegion = this.arrBirds;
+        console.log(this.arrBirdsRegion);
+        // console.log(this.arrBirdsRegion);
+        // this.arrBirds.sort((name,level) => (name.level - level.id));
+        // //  console.log(this.arrBirds[1]);
+      },
+      (err: HttpErrorResponse) => {
+        console.log (err.message);
+      }
+    );
+  }
 }
